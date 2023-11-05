1ab71badd569df0bd3290d6ea3317af534a3f272
amyzliao <amzhliao@gmail.com>
2023-11-05 17:22:43 -0600

    shadows

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -3,20 +3,9 @@ import Navbar from '../../components/navbar'
 export default function TestingPage() {
     return (
         <main >
-            <a href='https://www.google.com'>Google.com</a>
-            <button>Click me</button>
-
-            <ul>
-                <li>This is element 1</li>
-                <li>This is element 2</li>
-                <li>This is element 3</li>
-                <li>This is element 4</li>
-                <li>This is element 5</li>
-                <li>This is element 6</li>
-                <li>This is element 7</li>
-                <li>This is element 8</li>
-                <li>This is element 9</li>
-            </ul>
+            <h1>
+                Welcome to my website
+            </h1>
         </main>
     )
 }
