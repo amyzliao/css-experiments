9b5ea8fffa343edd0f8dac01d1a58e11ed4727a3
amyzliao <amzhliao@gmail.com>
2023-11-05 16:47:37 -0600

    margins!

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -3,9 +3,11 @@ import Navbar from '../../components/navbar'
 export default function TestingPage() {
     return (
         <main >
-            <h1 >AMY LIAO</h1>
-            <p id='p1' class='odd'>I'm not comfortable accepting donations for my work. Rather, I would like to encourage you to donate to a struggling family or a charity of your choosing. One charity I support is St. Jude's Hospital, which covers the cost of treatment for kids with cancer and other illnesses. There is always somebody who is in a worse situation than us. We should do our part to help if we can. Thanks everyone</p>
-            <p id='p2' class='even'>I'm not comfortable accepting donations for my work. Rather, I would like to encourage you to donate to a struggling family or a charity of your choosing. One charity I support is St. Jude's Hospital, which covers the cost of treatment for kids with cancer and other illnesses. There is always somebody who is in a worse situation than us. We should do our part to help if we can. Thanks everyone</p>
+            {/* <h1 >AMY LIAO</h1> */}
+            <div id='box1'>I'm not comfortable accepting donations for my work. Rather, I would like to encourage you to donate to a struggling family or a charity of your choosing. One charity I support is St. Jude's Hospital, which covers the cost of treatment for kids with cancer and other illnesses. There is always somebody who is in a worse situation than us. We should do our part to help if we can. Thanks everyone</div>
+            <div id='box2'>I'm not comfortable accepting donations for my work. Rather, I would like to encourage you to donate to a struggling family or a charity of your choosing. One charity I support is St. Jude's Hospital, which covers the cost of treatment for kids with cancer and other illnesses. There is always somebody who is in a worse situation than us. We should do our part to help if we can. Thanks everyone</div>
+            {/* <p id='p1' class='odd'>I'm not comfortable accepting donations for my work. Rather, I would like to encourage you to donate to a struggling family or a charity of your choosing. One charity I support is St. Jude's Hospital, which covers the cost of treatment for kids with cancer and other illnesses. There is always somebody who is in a worse situation than us. We should do our part to help if we can. Thanks everyone</p>
+            <p id='p2' class='even'>I'm not comfortable accepting donations for my work. Rather, I would like to encourage you to donate to a struggling family or a charity of your choosing. One charity I support is St. Jude's Hospital, which covers the cost of treatment for kids with cancer and other illnesses. There is always somebody who is in a worse situation than us. We should do our part to help if we can. Thanks everyone</p> */}
 
         </main>
     )
