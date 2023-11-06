8a1fd06289bd267e4312fd93fc4db8c0394a92fb
amyzliao <amzhliao@gmail.com>
2023-11-05 21:09:26 -0600

    fontawesome icons!

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,4 +1,8 @@
 import Navbar from '../../components/navbar'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHouse } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import '../globals.css'
 
 export default function TestingPage() {
     return (
@@ -6,6 +10,27 @@ export default function TestingPage() {
             <h1>
                 Welcome to my website
             </h1>
+            <a href="https://mail.google.com/">
+                <FontAwesomeIcon
+                    className="icon"
+                    id='fa-envelope'
+                    icon={faEnvelope}
+                    size='xs' />
+            </a>
+            <a href="./">
+                <FontAwesomeIcon
+                    className="icon"
+                    id="fa-house"
+                    icon={faHouse}
+                    size='xs' />
+            </a>
+            <a href="https://twitter.com/?lang=en">
+                <FontAwesomeIcon
+                    className="icon"
+                    id="fa-twitter"
+                    icon={faTwitter}
+                    size='xs' />
+            </a>
         </main>
     )
 }
